0d4520b41c64c577eeb60714188f86c395bbf07b
smile <134200591+smileygames@users.noreply.github.com>
2023-06-12 17:51:53 +0900

    up

--- README.md
+++ README.md
@@ -1,2 +1,126 @@
-# dipper
-DDNSにipを自動でupdateするシェルスクリプト
+# dipper (multi DDNS & IPv6 & multi domain対応)
+
+旧名称：mydns-ip-update
+
+MyDNS®JP → https://www.mydns.jp/
+
+オブジェクト指向化しました。（独自解釈）→ https://smgjp.com/mydns-ip-update_object/
+
+<br>
+
+## 概要
+- このスクリプトは、DDNSへの自動通知を目的としています。
+- 使用する環境はAlmaLinuxで、言語はBashです。
+- スクリプトは複数のファイルで構成されており、それぞれの役割や目的があります。
+- `config`ディレクトリ内の設定ファイルに基づいて動作します。
+- IPアドレスの更新やチェックを定期的に行います。
+
+<br>
+
+動作に関しては、簡易デバッグテストしかしてないので、ご了承ください。
+
+MyDNSを使用していて固定IPの場合は、confファイルでIPV4_DDNS及びIPV6_DDNSを「off」にしておいてください。（余計な処理をしなくなる）
+
+<br>
+
+## ワンクリックインストールスクリプト
+### インストールコマンド
+```bash
+bash <( curl -fsSL https://github.com/smileygames/dipper/releases/download/v2.10/install.sh )
+```
+
+<br>
+
+▼最初に初期設定を行ってください。
+
+installのたびにコンフィグファイルが初期値に戻ってしまうのも面倒なので
+ユーザー側でコンフィグファイルを作成してもらい、上書きインストールでも変更しないようにしました。
+但し、uninstallコマンドを実行すると消えます。
+```bash
+sudo cp -v /usr/local/dipper/config/default.conf /usr/local/dipper/config/user.conf
+sudo vim /usr/local/dipper/config/user.conf
+```
+```bash
+MYDNS_ID[1]=""
+MYDNS_PASS[1]=""
+MYDNS_DOMAIN[1]=""
+```
+をご自分のMyDNSの情報に書き換えて、先頭の#を削除してください。
+
+編集が終わったら権限を変更しておきます。（IDとPASSを管理したファイルの為）
+```bash
+sudo chmod 600 /usr/local/dipper/config/user.conf
+```
+
+<br>
+
+▼次にサービスの起動です。
+
+```bash
+sudo systemctl enable dipper.service --now
+```
+<br>
+
+### アンインストールスクリプト
+▼アンインストールコマンド
+```bash
+bash <( curl -fsSL https://github.com/smileygames/dipper/releases/download/v2.10/uninstall.sh )
+```
+
+<br>
+
+### 設定変更時
+コンフィグファイルの内容を変更した際は、
+サービスを再起動しないと反映されないので注意です。
+```bash
+sudo systemctl restart dipper.service
+```
+<br>
+
+## マニュアルインストール方法
+
+### ダウンロード及び権限の変更
+
+```bash
+Ver="2.10"
+wget https://github.com/smileygames/dipper/archive/refs/tags/v${Ver}.tar.gz -O - | sudo tar zxvf - -C ./
+sudo mv -fv dipper-${Ver} dipper
+sudo cp -rv dipper /usr/local/
+sudo rm -rf dipper
+sudo chmod -R 755 /usr/local/dipper/bin
+```
+
+### サービス作成
+```bash
+sudo vi /etc/systemd/system/dipper.service
+```
+```bash
+[Unit]
+Description=ddns-ip-upper
+
+[Service]
+Type=simple
+Restart=on-failure
+WorkingDirectory=/usr/local/dipper/bin
+ExecStart=/usr/local/dipper/bin/ip_update.sh
+
+[Install]
+WantedBy=network-online.target
+```
+
+### デーモンリロードをして追加したサービスを読み込ませて起動させる
+```bash
+sudo systemctl daemon-reload
+sudo systemctl enable dipper.service --now
+```
+<br>
+
+## スクリプト構成
+
+自分なりの解釈のオブジェクト指向もどきで作り直しました。
+
+言語はそのままシェルスクリプトです。
+
+機能は変わりません。
+
+![mydns-ip-update：スクリプト構成図 (2)](https://github.com/smileygames/mydns-ip-update/assets/134200591/19b532f9-8e56-4132-a04e-c18ee4430053)
--- bin/ddns_timer/multi_domain.sh
+++ bin/ddns_timer/multi_domain.sh
@@ -2,26 +2,37 @@
 #
 # ./ddns_timer/multi_domain.sh
 #
-# MyDNS
+# multi_domain.sh
 
 Mode=$1
-Login_URL=$2
-IP_Version=$3
-DNS_Record=$4
+IP_Version=$2
+DNS_Record=$3
+
+ip_url_set() {
+    if [ "$IP_Version" = 4 ]; then
+        Login_URL=${MYDNS_IPV4_URL}
+    elif [ "$IP_Version" = 6 ]; then
+        Login_URL=${MYDNS_IPV6_URL}
+    fi
+}
 
 # 配列のデータを読み込んでDDNSへアクセス
 multi_domain_ip_update() {
+    ip_url_set
     for i in "${!MYDNS_ID[@]}"; do
         if [[ ${MYDNS_ID[$i]} = "" ]] || [[ ${MYDNS_PASS[$i]} = "" ]]; then
             ./err_message.sh "no_value" "${FUNCNAME[0]}" "MYDNS_ID[$i] or MYDNS_PASS[$i]"
             continue
         fi
-        ./dns_access.sh "mydns" "$i" "${MYDNS_ID[$i]}:${MYDNS_PASS[$i]} ${Login_URL}"
+        ./dns_access.sh "MYDNS" "${FUNCNAME[0]}" "$i" "${MYDNS_ID[$i]}:${MYDNS_PASS[$i]} ${Login_URL}"
     done
 }
 
 # 配列のデータを読み込んでアドレスをチェックし変更があった場合のみ、DDNSへアクセス
-multi_domain_mydns_check() {
+mydns_multi_domain_check() {
+    IP_New=$1
+
+    ip_url_set
     for i in "${!MYDNS_ID[@]}"; do
         if [[ ${MYDNS_ID[$i]} = "" ]] || [[ ${MYDNS_PASS[$i]} = "" ]] || [[ ${MYDNS_DOMAIN[$i]} = "" ]]; then
             ./err_message.sh "no_value" "${FUNCNAME[0]}" "MYDNS_ID[$i] or MYDNS_PASS[$i] or MYDNS_DOMAIN[$i]"
@@ -30,9 +41,49 @@ multi_domain_mydns_check() {
         IP_old=$(dig "${MYDNS_DOMAIN[$i]}" "$DNS_Record" +short)  # ドメインのアドレスを読み込む
 
         if [[ "$IP_New" != "$IP_old" ]]; then
-            ./dns_access.sh "mydns" "$i" "${MYDNS_ID[$i]}:${MYDNS_PASS[$i]} ${Login_URL}"
+            ./dns_access.sh "MYDNS" "${FUNCNAME[0]}" "$i" "${MYDNS_ID[$i]}:${MYDNS_PASS[$i]} ${Login_URL}"
          fi
     done
+}
+
+# Googleの場合用のDDNSアクセス
+google_multi_domain_check() {
+    IP_New=$1
+
+    for i in "${!GOOGLE_ID[@]}"; do
+        if [[ ${GOOGLE_ID[$i]} = "" ]] || [[ ${GOOGLE_PASS[$i]} = "" ]] || [[ ${GOOGLE_DOMAIN[$i]} = "" ]]; then
+            ./err_message.sh "no_value" "${FUNCNAME[0]}" "GOOGLE_ID[$i] or GOOGLE_PASS[$i] or GOOGLE_DOMAIN[$i]"
+            continue
+        fi 
+        if [ "$IP_Version" = 4 ] && [[ ${GOOGLE_IPV6[$i]} = on ]]; then
+            continue
+        elif [ "$IP_Version" = 6 ] && [[ ${GOOGLE_IPV6[$i]} = off ]]; then
+            continue
+        fi
+        IP_old=$(dig "${GOOGLE_DOMAIN[$i]}" "$DNS_Record" +short)  # ドメインのアドレスを読み込む
+
+        if [[ "$IP_New" != "$IP_old" ]]; then
+            ./dns_access.sh "GOOGLE" "${FUNCNAME[0]}" "$i" "${GOOGLE_ID[$i]}:${GOOGLE_PASS[$i]} ${GOOGLE_URL}?hostname=${GOOGLE_DOMAIN[$i]}&myip=${IP_New}"
+        fi
+    done
+}
+
+# 複数のDDNSサービス用
+multi_ddns_check() {
+    MyIP=$(dig @ident.me -"$IP_Version" +short)  # 自分のアドレスを読み込む
+    if [[ $MyIP = "" ]]; then
+        ./err_message.sh "no_value" "${FUNCNAME[0]}" "自分のIPアドレスを取得できなかった"
+        return 1
+    fi
+
+    if [ ${#MYDNS_ID[@]} != 0 ]; then
+        mydns_multi_domain_check "$MyIP" &
+    fi
+
+    # GoogleのDDNSサービスはIPv4とIPv6が排他制御のための処理
+    if [ ${#GOOGLE_ID[@]} != 0 ]; then
+        google_multi_domain_check "$MyIP" &
+    fi
 }
 
 # 実行スクリプト
@@ -41,12 +92,7 @@ case ${Mode} in
         multi_domain_ip_update
         ;;
    "check") 
-        IP_New=$(dig @ident.me -"$IP_Version" +short)  # 自分のアドレスを読み込む
-        if [[ $IP_New = "" ]]; then
-            ./err_message.sh "no_value" "${FUNCNAME[0]}" "自分のIPアドレスを取得できなかった"
-            return 1
-        fi
-        multi_domain_mydns_check
+        multi_ddns_check
         ;;
     * )
         echo "[${Mode}] <- 引数エラーです"
