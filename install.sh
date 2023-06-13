322c828fe41deff7b9a7f59cfc5f55bffabe3900
smile <134200591+smileygames@users.noreply.github.com>
2023-06-13 15:49:37 +0900

    #10 サービスの状態をみて処理を分岐させるように変更

--- install.sh
+++ install.sh
@@ -5,10 +5,21 @@
 # dipper
 
 Ver="1.02"
+SERVICE_NAME="dipper.service"
 
-# サービスの停止
-sudo systemctl stop dipper.service
-sudo systemctl disable dipper.service
+# サービスの状態を確認
+status=$(systemctl is-active "$SERVICE_NAME")
+# 状態に応じて処理を分岐
+if [ "$status" = "active" ]; then
+    systemctl stop "$SERVICE_NAME"
+fi
+
+# サービスの有効化状態を確認
+enabled=$(systemctl is-enabled "$SERVICE_NAME")
+# 有効化状態に応じて処理を分岐
+if [ "$enabled" = "enabled" ]; then
+    systemctl disable "$SERVICE_NAME"
+fi
 
 # 以前のバージョンのアンインストール処理
 sudo rm -rf /usr/local/dipper/bin
--- uninstall.sh
+++ uninstall.sh
@@ -4,9 +4,22 @@
 #
 # dipper 
 
-# v1.00以降用
-sudo systemctl stop dipper.service
-sudo systemctl disable dipper.service
+SERVICE_NAME="dipper.service"
+
+# サービスの状態を確認
+status=$(systemctl is-active "$SERVICE_NAME")
+# 状態に応じて処理を分岐
+if [ "$status" = "active" ]; then
+    systemctl stop "$SERVICE_NAME"
+fi
+
+# サービスの有効化状態を確認
+enabled=$(systemctl is-enabled "$SERVICE_NAME")
+# 有効化状態に応じて処理を分岐
+if [ "$enabled" = "enabled" ]; then
+    systemctl disable "$SERVICE_NAME"
+fi
+
  # 以前のバージョン用
 sudo rm -f /etc/systemd/system/dipper.service
 
