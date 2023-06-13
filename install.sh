16a27433e30d418ea3a259ef681a4b8349657c1b
smile <134200591+smileygames@users.noreply.github.com>
2023-06-13 15:26:16 +0900

    #10 実態を置かずにシンボリックリンクに変更。

--- install.sh
+++ install.sh
@@ -17,12 +17,12 @@ sudo rm -rf /usr/local/dipper/bin
 # スクリプトファイルダウンロード＆ファイル属性変更
 wget https://github.com/smileygames/dipper/archive/refs/tags/v${Ver}.tar.gz -O - | sudo tar zxvf - -C ./
 sudo mv -fv dipper-${Ver} dipper
-sudo cp -v dipper/systemd/dipper.service /etc/systemd/system/
-sudo rm -rf dipper/systemd
 sudo cp -rv dipper /usr/local/
 sudo rm -rf dipper
 
 sudo chmod -R 755 /usr/local/dipper/bin
+sudo ln -s /usr/local/dipper/systemd/dipper.service /etc/systemd/system
 
 # デーモンリロードをして追加したサービスを読み込ませる
 sudo systemctl daemon-reload
+
