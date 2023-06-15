f902c5b6d2fe22513e3948721f3e51df9e219d45
smile <134200591+smileygames@users.noreply.github.com>
2023-06-15 09:20:30 +0900

    #28 curl errorの通常メッセージによる誤作動の修正

--- bin/dns_access.sh
+++ bin/dns_access.sh
@@ -12,7 +12,7 @@ Access_URL=$4
 Out_Time=25s
 Max_Time=21
 
-accsse() {
+access() {
     # DDNSへアクセスするがIDやパスワードがおかしい場合、対話式モードになってスタックするのでタイムアウト処理を入れている
     output=$(timeout ${Out_Time} curl --max-time ${Max_Time} -sSu ${Access_URL} 2>&1)
     exit_code=$?
@@ -20,7 +20,7 @@ accsse() {
     if [ "${exit_code}" != 0 ]; then
         # タイムアウトエラー
         ./err_message.sh "timeout" "${Func_Name}" "${Out_Time}: ログイン情報 curl -u ${Mode}_ID[$Array_Num]:${Mode}_PASS[$Array_Num]  URL"
-    elif [[ "${output}" == *"html"* ]]; then
+    elif [[ "${output}" == *'<p>'* ]]; then
         # curlコマンドのエラーメッセージの抽出（<title>内のテキスト）
         error_title=$(echo "${output}" | grep -o '<title>[^<]*</title>' | sed 's/<[^>]*>//g')
         # curlコマンドのエラーメッセージの抽出（<p>内のテキスト）
@@ -33,4 +33,4 @@ accsse() {
 }
 
 # 実行スクリプト
-accsse
+access
