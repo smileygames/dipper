d89b4783873bf1868163bb16d09ccbda9942ed8c
smile <134200591+smileygames@users.noreply.github.com>
2023-06-15 11:39:23 +0900

    #28 timeoutの時間を変更

--- bin/dns_access.sh
+++ bin/dns_access.sh
@@ -9,18 +9,18 @@ Func_Name=$2
 Array_Num=$3
 Access_URL=$4
 
-Max_Time=21
+Max_Time=25
 
 access() {
     # DDNSへアクセスするがIDやパスワードがおかしい場合、対話式モードになってスタックするので"-f"処理を入れている
-    output=$(curl --max-time ${Max_Time} -fsSu ${Access_URL} 2>&1)
+    output=$(curl --max-time ${Max_Time} -sSfu ${Access_URL} 2>&1)
     exit_code=$?
 
     if [ "${exit_code}" != 0 ]; then
         # curlコマンドのエラー
         ./err_message.sh "curl" "${Func_Name}" "${Mode}_ID[$Array_Num]:${Mode}_PASS[$Array_Num]: ${output}"
     else
-        echo "Access successful"
+        echo "Access successful ${Func_Name}: curl ${Mode}_ID[$Array_Num]:${Mode}_PASS[$Array_Num]"
     fi
 }
 
