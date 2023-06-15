001633129cb8305ce265f25d2e41f2c2559a816e
smile <134200591+smileygames@users.noreply.github.com>
2023-06-15 10:53:56 +0900

    #28 timeoutの代わりにcurlオプションでスタックしないように変更

--- bin/dns_access.sh
+++ bin/dns_access.sh
@@ -9,24 +9,16 @@ Func_Name=$2
 Array_Num=$3
 Access_URL=$4
 
-Out_Time=25s
 Max_Time=21
 
 access() {
-    # DDNSへアクセスするがIDやパスワードがおかしい場合、対話式モードになってスタックするのでタイムアウト処理を入れている
-    output=$(timeout ${Out_Time} curl --max-time ${Max_Time} -sSu ${Access_URL} 2>&1)
+    # DDNSへアクセスするがIDやパスワードがおかしい場合、対話式モードになってスタックするので"-f"処理を入れている
+    output=$(curl --max-time ${Max_Time} -fsSu ${Access_URL} 2>&1)
     exit_code=$?
 
     if [ "${exit_code}" != 0 ]; then
-        # タイムアウトエラー
-        ./err_message.sh "timeout" "${Func_Name}" "${Out_Time}: ログイン情報 curl -u ${Mode}_ID[$Array_Num]:${Mode}_PASS[$Array_Num]  URL"
-    elif [[ "${output}" == *'<p>'* ]]; then
-        # curlコマンドのエラーメッセージの抽出（<title>内のテキスト）
-        error_title=$(echo "${output}" | grep -oE '<title>([^<]*)</title>' | sed -E 's/<[^>]*>//g')
-        # curlコマンドのエラーメッセージの抽出（<p>内のテキスト）
-        error_message=$(echo "${output}" | grep -oE '<p>([^<]*)</p>' | sed -E 's/<[^>]*>//g')
         # curlコマンドのエラー
-        ./err_message.sh "curl" "${Func_Name}" "${Mode}_ID[$Array_Num]:${Mode}_PASS[$Array_Num]: ${error_title}: ${error_message}"
+        ./err_message.sh "curl" "${Func_Name}" "${Mode}_ID[$Array_Num]:${Mode}_PASS[$Array_Num]: ${output}"
     else
         echo "Access successful"
     fi
