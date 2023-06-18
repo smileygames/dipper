da088343bd76a447b30202f9b659a4efe0be9448
smile <134200591+smileygames@users.noreply.github.com>
2023-06-18 20:10:54 +0900

    #39 output変数を処理後に開放してみる。

--- bin/dns_access.sh
+++ bin/dns_access.sh
@@ -12,7 +12,6 @@ Access_URL=$4
 Max_Time=30
 
 access() {
-    local output=""
     # DDNSへアクセスするがIDやパスワードがおかしい場合、対話式モードになってスタックするので"-f"処理を入れている
     output=$(curl --max-time ${Max_Time} -sSfu ${Access_URL} 2>&1)
     exit_code=$?
@@ -23,6 +22,7 @@ access() {
     else
         echo "Access successful ${output}: ${Func_Name}: curl ${Mode}_ID[$Array_Num]:${Mode}_PASS[$Array_Num]"
     fi
+    unset output
 }
 
 # 実行スクリプト
