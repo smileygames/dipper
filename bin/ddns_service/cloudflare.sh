31124445a2fedd14d1044fa486d5b757c005d710
smile <134200591+smileygames@users.noreply.github.com>
2024-02-02 02:37:03 +0900

    とりあえず動いたが共通化できそうなので暫定版

--- bin/ddns_service/cloudflare.sh
+++ bin/ddns_service/cloudflare.sh
@@ -27,6 +27,10 @@ ip_check_v4_v6() {
             ./err_message.sh "no_value" "${FUNCNAME[0]}" "自分のIPv4アドレスを取得できなかった"
         else
             IPv4_old=$(dig "${CLOUDFLARE_DOMAIN[$i]}" "A" +short)  # ドメインのアドレスを読み込む
+            if [[ "$My_ipv4" != "$IPv4_old" ]]; then
+                # バックグラウンドプロセスで実行
+                ./dns_api_access.sh "ipv4" "$api" "$mail" "$zone" "$domain" "$My_ipv4" &
+            fi
         fi
     fi
     if [ "$IPv6_ddns" = on ]; then
@@ -34,23 +38,12 @@ ip_check_v4_v6() {
         if [[ $My_ipv6 = "" ]]; then
             ./err_message.sh "no_value" "${FUNCNAME[0]}" "自分のIPv6アドレスを取得できなかった"
         else
-            IPv6_old=$(dig "${CLOUDFLARE_DOMAIN[$i]}" "AAAAA" +short)  # ドメインのアドレスを読み込む
+            IPv6_old=$(dig "${CLOUDFLARE_DOMAIN[$i]}" "AAAA" +short)  # ドメインのアドレスを読み込む
+            if [[ "$My_ipv6" != "$IPv6_old" ]]; then
+                # バックグラウンドプロセスで実行
+                ./dns_api_access.sh "ipv6" "$api" "$mail" "$zone" "$domain" "$My_ipv6" &
+            fi
         fi
-    fi
-
-    if [[ "$IPv4_ddns" = on ]] && [[ "$IPv6_ddns" = on ]]; then
-        if [[ "$My_ipv4" != "$IPv4_old" ]] || [[ "$My_ipv6" != "$IPv6_old" ]]; then
-            # バックグラウンドプロセスで実行
-            ./dns_api_access.sh "ipv4_ipv6" "$api" "$mail" "$zone" "$domain" "$My_ipv4" "$My_ipv6" &
-        fi
-    fi
-    if [[ "$My_ipv4" != "$IPv4_old" ]]; then
-        # バックグラウンドプロセスで実行
-        ./dns_api_access.sh "ipv4" "$api" "$mail" "$zone" "$domain" "$My_ipv4" &
-    fi
-    if [[ "$My_ipv6" != "$IPv6_old" ]]; then
-        # バックグラウンドプロセスで実行
-        ./dns_api_access.sh "ipv6" "$api" "$mail" "$zone" "$domain" "$My_ipv6" &
     fi
 }
 
@@ -64,9 +57,7 @@ cloudflare_multi_domain_check() {
             continue
         fi
 
-        if [ "$IP_Version" = 4 ] && [[ ${CLOUDFLARE_IPV4[$i]} != on ]]; then
-            continue
-        elif [ "$IP_Version" = 6 ] && [[ ${CLOUDFLARE_IPV6[$i]} != on ]]; then
+        if [[ ${CLOUDFLARE_IPV4[$i]} != on ]] && [[ ${CLOUDFLARE_IPV6[$i]} != on ]]; then
             continue
         fi
         ip_check_v4_v6 "${CLOUDFLARE_IPV4[$i]}" "${CLOUDFLARE_IPV6[$i]}" "${CLOUDFLARE_API[$i]}" "${CLOUDFLARE_MAIL[$i]}" "${CLOUDFLARE_ZONE[$i]}" "${CLOUDFLARE_DOMAIN[$i]}"
