cf7b01f79d586e936fd57b21e7cf3c2183ec3a05
smile <134200591+smileygames@users.noreply.github.com>
2024-02-15 00:02:53 +0900

    #93 ループ処理のタイミングの変更 エラーメールチェック

--- bin/err_mail_service.sh
+++ bin/err_mail_service.sh
@@ -11,10 +11,9 @@ main() {
     local wait_time=""
 
     wait_time=$(./time_check.sh "error" "$Check_Time")
-    # 最初の起動を行う
-    ./mail_handle.sh "err_mail" "dipperでエラーを検出しました" "$Email_Adr" & 
     while true;do
-        sleep "$wait_time";./mail_handle.sh "err_mail" "dipperでエラーを検出しました" "$Email_Adr" & 
+        ./mail_handle.sh "err_mail" "dipperでエラーを検出しました" "$Email_Adr" &
+        sleep "$wait_time" 
     done
 }
 
